5e3e74417bb6c1cd606ecf15deef61c061d143c1
Lazaro Lironis <99282167+LazaroLironis241103@users.noreply.github.com>
2025-09-30 20:41:05 +0000

    Barra de Navegacion en Control de Clientes

--- Angular/control-clientes/src/app/app.component.ts
+++ Angular/control-clientes/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabeceroComponent } from "./componentes/cabecero.component/cabecero.component";
+import { PiePaginaComponent } from "./componentes/pie-pagina.component/pie-pagina.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabeceroComponent, PiePaginaComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- Angular/control-clientes/src/app/app.routes.ts
+++ Angular/control-clientes/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { TableroComponent } from './componentes/tablero.component/tablero.component';
+import { LoginComponent } from './componentes/login.component/login.component';
+import { EditarClienteComponent } from './componentes/editar-cliente.component/editar-cliente.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado.component/no-encontrado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: TableroComponent}, //localhost:4200
+    {path: 'login', component: LoginComponent},
+    {path: 'cliente/editar/:id', component: EditarClienteComponent},
+    {path: '**', component: NoEncontradoComponent}
+];
