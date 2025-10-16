d6eb88faeda6430cca1f5ab31405ad543d19f391
Lazaro Lironis <99282167+LazaroLironis241103@users.noreply.github.com>
2025-10-16 20:12:18 +0000

    Login parte 1

--- Angular/control-clientes/src/app/componentes/login.component/login.component.ts
+++ Angular/control-clientes/src/app/componentes/login.component/login.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login.component',
-  imports: [],
+  selector: 'app-login',
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email: string | null = null;
+  password: string | null = null;
 
+  constructor(private router: Router) {}
+
+  login() {
+throw new Error('Method not implemented.');
+}
 }
